96239c752bc5fc92a4e362b5be52f0c392f5211c
Lytvynenko Denys <den.litvinenko@gmail.com>
2016-01-09 22:32:20 +0200

    add new lines to file

--- src/UMLDiagram.java
+++ src/UMLDiagram.java
@@ -9,21 +9,22 @@ public class UMLDiagram {
     public static String filePath;
     public static String fileName;
     public static String fullFilePath;
+    public static String text;
     public static void main (String[] args) throws IOException {
         filePath = args[0];
-        System.out.printf("File path is: %s", filePath);
+        System.out.printf("File path is: %s\n", filePath);
         fileName = args[1];
-        System.out.printf("File name is: %s", fileName);
-        fullFilePath = filePath;
-
+        System.out.printf("File name is: %s\n", fileName);
+        //fullFilePath = filePath;
+         text = args[3];
         StringBuffer sb = new StringBuffer();
-        fullFilePath = sb.append(fileName).toString();
-        System.out.printf("Full file name is: %s", fullFilePath);
+        fullFilePath = sb.append(filePath).append("\\").append(fileName).toString();
+        System.out.printf("Full file name is: %s\n", fullFilePath);
         if (args[2].equals("txt")){
             Directory dir = new Directory();
             dir.createDirectory(filePath);
             MyFile mFile = new Text();
-            mFile.createMyFile(fullFilePath);
+            mFile.createMyFile(fullFilePath, text);
         } else {
             System.out.println("Unknown file extension");
         }
